8a393d55a129901211816a64eaaba6ff3bb916c0
alexisthethe <alexisthethe@gmail.com>
2024-12-09 11:19:31 +0100

    declare variables

--- main.go
+++ main.go
@@ -8,9 +8,28 @@ func main() {
 	fmt.Println("#######################")
 	fmt.Println("#### My TODO List! ####")
 	fmt.Println("#######################")
+
+	var taskDishes = "Do the dishes"
+	var taskLaundry = "Finish the laundry"
+	taskCarrots := "Buy 3 carrots"
+
 	fmt.Println()
-	fmt.Println("1. Do the dishes")
-	fmt.Println("2. Finish the laundry")
-	fmt.Println("3. Clean the house")
+	fmt.Println("All tasks:")
+	fmt.Println("- " + taskDishes)
+	fmt.Println("- " + taskLaundry)
+	fmt.Println("- " + taskCarrots)
+
+	fmt.Println()
+	fmt.Println("Priority:")
+	fmt.Println("- " + taskDishes)
+
+	fmt.Println()
+	fmt.Println("Home work:")
+	fmt.Println("- " + taskDishes)
+	fmt.Println("- " + taskLaundry)
+
+	fmt.Println()
+	fmt.Println("Groceries:")
+	fmt.Println("- " + taskCarrots)
 
 }
